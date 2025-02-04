035ce29f0999be794305dbddf14c78f92ee870c1
Sidi <hafdalamehdisidi@gmail.com>
2025-02-04 12:49:33 +0100

    Create classs and methods to triangle2

---
+++ code/src/main/java/com/endes/figuras/Triangulo_2.java
@@ -0,0 +1,16 @@
+package com.endes.figuras;
+
+public class Triangulo_2 {
+Punto vertice1;
+Punto vertice2;
+Punto vertice3;
+
+public static triangulo_2 (String nombre, String color, punto v1, punto v2, punto v3); {
+	
+}
+public static double area();
+public static double perimetro();
+public static void rotar(double angulo);
+public static void trasladar(double dx, double dy);
+
+}
